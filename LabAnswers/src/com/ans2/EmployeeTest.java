943ee1813cfb848229763e06f16c537d27900f5c
user <user@LAPTOP-9L8LH19B>
2022-10-13 18:52:46 +0530

    13.10.2022 lab answers

--- LabAnswers/src/com/ans2/EmployeeTest.java
+++ LabAnswers/src/com/ans2/EmployeeTest.java
@@ -1,14 +1,21 @@
 package com.ans2;
 
+// Declaring a class
 public class EmployeeTest {
+
+	// declaring a main method
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		// creating object of salariedemployee
 		SalariedEmployee employee1 = new SalariedEmployee(102, "Sujoy", 45);
-		System.out.println(employee1);
+		System.out.println(employee1); // printing
 
+		// creating object of Hourlyemployee
 		HourlyEmployee employee2 = new HourlyEmployee(104, "Dipa", 33);
-		System.out.println(employee2);
-	}
+		System.out.println(employee2); // printing
 
-}
+	} // end a main method
+
+}// end of the class
